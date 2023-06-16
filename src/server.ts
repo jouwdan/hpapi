89071d1b704aaca6966da6be084c25566ae70767
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-16 15:43:55 +0100

    feat: add movies route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import fastify from 'fastify';
 import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config';
-import { BooksQuerystring, CharacterQuerystring } from './types';
+import { BooksQuerystring, CharacterQuerystring, MoviesQuerystring } from './types';
 
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
   throw new Error('Supabase credentials not found in environment variables');
@@ -170,6 +170,83 @@ server.get<{
 
   const { data: books, error } = await query;
   return { data: books, error };
+});
+
+server.get<{
+  Querystring: MoviesQuerystring;
+}>('/movies', async (req, res) => {
+  const {
+    id,
+    title,
+    summary,
+    directors,
+    screenwriters,
+    producers,
+    cinematographers,
+    editors,
+    distributors,
+    music_composers,
+    release_date,
+    running_time,
+    budget,
+    box_office,
+    rating,
+    trailer,
+    poster,
+    wiki,
+    sort = 'title',
+    order = 'asc',
+    page,
+    size,
+  } = req.query;
+
+  let query = supabase.from('movies').select('*');
+
+  const filterField = (field: any, value: any) => {
+    if (value) {
+      query = query.filter(field, 'ilike', `%${value}%`);
+    }
+  };
+
+  filterField('id', id);
+  filterField('title', title);
+  filterField('summary', summary);
+  filterField('directors', directors);
+  filterField('screenwriters', screenwriters);
+  filterField('producers', producers);
+  filterField('cinematographers', cinematographers);
+  filterField('editors', editors);
+  filterField('distributors', distributors);
+  filterField('music_composers', music_composers);
+  filterField('release_date', release_date);
+  filterField('running_time', running_time);
+  filterField('budget', budget);
+  filterField('box_office', box_office);
+  filterField('rating', rating);
+  filterField('trailer', trailer);
+  filterField('poster', poster);
+  filterField('wiki', wiki);
+
+  if (order) {
+    if (order === 'asc') {
+      query = query.order(sort);
+    } else if (order === 'desc') {
+      query = query.order(sort, { ascending: false });
+    } else {
+      throw new Error('order must be either asc or desc');
+    }
+  }
+
+  if (page || size) {
+    if (page && size) {
+      query = query.range(page - 1, page * size - 1);
+    } else {
+      throw new Error('page and size must be provided together');
+    }
+  }
+
+  const { data: movies, error } = await query;
+  return { data: movies, error };
 });
 
 const port = process.env.NODE_ENV === 'prod' ? 443 : 8080;
--- src/types.ts
+++ src/types.ts
@@ -47,3 +47,28 @@ export type BooksQuerystring = {
   page?: number;
   size?: number;
 }
+
+export type MoviesQuerystring = {
+  id?: string;
+  title?: string;
+  summary?: string;
+  directors?: string;
+  screenwriters?: string;
+  producers?: string;
+  cinematographers?: string;
+  editors?: string;
+  distributors?: string;
+  music_composers?: string;
+  release_date?: string;
+  running_time?: string;
+  budget?: string;
+  box_office?: string;
+  rating?: string;
+  trailer?: string;
+  poster?: string;
+  wiki?: string;
+  sort?: string;
+  order?: string;
+  page?: number;
+  size?: number;
+}
