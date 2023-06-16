4f4625fb099245d01c971ecc86d1c505a7d611d3
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-16 21:46:56 +0100

    fix: swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { join } from 'path';
 import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
 import { FastifyPluginAsync } from 'fastify';
+import Swagger from '@fastify/swagger'
+import SwaggerUI from '@fastify/swagger-ui'
 
 export type AppOptions = {} & Partial<AutoloadPluginOptions>;
 
@@ -10,6 +12,29 @@ const app: FastifyPluginAsync<AppOptions> = async (
     fastify,
     opts
 ): Promise<void> => {
+  void fastify.register(Swagger, {
+    swagger: {
+      info: {
+        title: 'Harry Potter API',
+        description: 'Harry Potter API documentation',
+        version: '0.1.0'
+      },
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      tags: [
+        { name: 'characters', description: 'Character related end-points' },
+        { name: 'potions', description: 'Potion related end-points' },
+        { name: 'spells', description: 'Spell related end-points' },
+        { name: 'books', description: 'Book related end-points' },
+        { name: 'movies', description: 'Movie related end-points' }
+      ],
+    },
+  })
+
+  void fastify.register(SwaggerUI, {
+    routePrefix: '/'
+  })
+
   void fastify.register(AutoLoad, {
     dir: join(__dirname, 'plugins'),
     options: opts
