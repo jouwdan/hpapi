3b4a02796128001c0ac03a4c1c3a4d78f8e499db
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-16 15:37:55 +0100

    feat: add books route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import fastify from 'fastify';
 import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config';
-import { CharacterQuerystring } from './types';
+import { BooksQuerystring, CharacterQuerystring } from './types';
 
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
   throw new Error('Supabase credentials not found in environment variables');
@@ -107,8 +107,69 @@ server.get<{
     }
   }
 
-  const { data: characters, error } = await query;
-  return { data: characters, error };
+  const { data: books, error } = await query;
+  return { data: books, error };
+});
+
+server.get<{
+  Querystring: BooksQuerystring;
+}>('/books', async (req, res) => {
+  const {
+    id,
+    title,
+    summary,
+    author,
+    release_date,
+    dedication,
+    pages,
+    cover,
+    wiki,
+    chapters,
+    sort = 'name',
+    order = 'asc',
+    page,
+    size,
+  } = req.query;
+
+  let query = supabase.from('characters').select('*');
+
+  const filterField = (field: any, value: any) => {
+    if (value) {
+      query = query.filter(field, 'ilike', `%${value}%`);
+    }
+  };
+
+  filterField('id', id);
+  filterField('title', title);
+  filterField('summary', summary);
+  filterField('author', author);
+  filterField('release_date', release_date);
+  filterField('dedication', dedication);
+  filterField('pages', pages);
+  filterField('cover', cover);
+  filterField('wiki', wiki);
+  filterField('chapters', chapters);
+
+  if (order) {
+    if (order === 'asc') {
+      query = query.order(sort);
+    } else if (order === 'desc') {
+      query = query.order(sort, { ascending: false });
+    } else {
+      throw new Error('order must be either asc or desc');
+    }
+  }
+
+  if (page || size) {
+    if (page && size) {
+      query = query.range(page - 1, page * size - 1);
+    } else {
+      throw new Error('page and size must be provided together');
+    }
+  }
+
+  const { data: books, error } = await query;
+  return { data: books, error };
 });
 
 const port = process.env.NODE_ENV === 'prod' ? 443 : 8080;
--- src/types.ts
+++ src/types.ts
@@ -30,4 +30,20 @@ export type CharacterQuerystring = {
     page?: number;
     size?: number;
 }
-  
+
+export type BooksQuerystring = {
+  id?: string;
+  title?: string;
+  summary?: string;
+  author?: string;
+  release_date?: string;
+  dedication?: string;
+  pages?: string;
+  cover?: string;
+  wiki?: string;
+  chapters?: string;
+  sort?: string;
+  order?: string;
+  page?: number;
+  size?: number;
+}
