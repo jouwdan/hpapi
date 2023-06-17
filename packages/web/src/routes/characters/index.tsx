0f43bba3d07b4830224e5e86c9852e86bdc07582
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-17 16:45:09 +0100

    fix: add search icon

--- packages/web/src/routes/characters/index.tsx
+++ packages/web/src/routes/characters/index.tsx
@@ -1,11 +1,11 @@
-import { SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { Box, Text, Image, SimpleGrid, useColorModeValue, Button, LinkBox, LinkOverlay, Select, Flex, ButtonGroup, Spacer, Center, InputGroup, Input } from '@chakra-ui/react';
-import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight } from 'react-icons/fi'
+import { Box, Text, Image, SimpleGrid, useColorModeValue, Button, LinkBox, LinkOverlay, Select, Flex, ButtonGroup, Spacer, Center, InputGroup, Input, InputLeftElement } from '@chakra-ui/react';
+import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight, FiSearch } from 'react-icons/fi'
 
 function Characters() {
   const [page, setPage] = useState(1);
-  const [size, setSize] = useState(10);
+  const [size, setSize] = useState(12);
   const [searchTerm, setSearchTerm] = useState('');
   const { data, status } = useQuery<any>([`/characters?page=${page}&size=${size}&name=${searchTerm}`]);
   const isLoading = status === 'loading';
@@ -17,7 +17,10 @@ function Characters() {
 
   return (
     <Box>
-        <Input placeholder="Search..." value={searchTerm} onChange={handleSearchTermInput} mb={4} />
+      <InputGroup mb={4}>
+        <InputLeftElement pointerEvents="none" children={<FiSearch />} />
+        <Input placeholder="Search..." value={searchTerm} onChange={handleSearchTermInput} />
+      </InputGroup>
       
       {isLoading ? (
         <div>Loading...</div>
@@ -74,9 +77,9 @@ function Characters() {
                   <Button colorScheme='blue' variant='outline' onClick={() => setPage(page + 10)}><FiChevronsRight /></Button>
                 </ButtonGroup>
               <Select variant='outline' value={size} w={20} ml={4} onChange={(e) => setSize(parseInt(e.target.value))}>
-                <option value={10}>10</option>
-                <option value={25}>25</option>
-                <option value={50}>50</option>
+                <option value={12}>12</option>
+                <option value={24}>24</option>
+                <option value={48}>48</option>
                 </Select>
               </Flex>
           </>
