b01d47780854e54ba08077a6c5d080561bafb29a
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-17 10:48:32 +0100

    feat: add components

---
+++ packages/web/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import { Box } from '@chakra-ui/react'
+
+interface NavbarProps {
+  currentPath: string
+}
+
+const Navbar: FC<NavbarProps> = ({ currentPath }) => {
+  
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default Navbar
