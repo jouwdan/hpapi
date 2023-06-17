6bf0fe7533c6732362ca73661a37a7175b151638
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-17 13:50:48 +0100

    feat: character boxes

--- packages/web/src/routes/characters/index.tsx
+++ packages/web/src/routes/characters/index.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { Box } from '@chakra-ui/react';
+import { Box, Text, Image, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
 
 async function fetchCharacters() {
   const res = await fetch('https://hpapi.onrender.com/characters?page=1&size=10');
@@ -19,6 +19,9 @@ function Characters() {
   const isLoading = status === 'loading';
   const error = status === 'error';
 
+
+  const bgColor = useColorModeValue('white', 'gray.800');
+
   return (
     <Box>
       {isLoading ? (
@@ -26,13 +29,29 @@ function Characters() {
       ) : error ? (
         <div>Error</div>
       ) : (
-            <div>
+            <SimpleGrid columns={3} spacing={4}>
               {data.data.map((character: any) => (
-                <div key={character.id}>
-                  <h1>{character.name}</h1>
-                </div>
+                <Box role={'group'}
+                  p={6}
+                  maxW={'330px'}
+                  w={'full'}
+                  bg={bgColor}
+                  boxShadow={'sm'}
+                  rounded={'lg'}
+                  pos={'relative'}
+                  zIndex={1}>
+                  <Image src={character.image} fallbackSrc='https://via.placeholder.com/150' boxSize='150px' mx='auto' my={8} alt={character.name} />
+                  <Text
+                    color={'gray.500'}
+                    fontWeight={600}
+                    fontSize={'sm'}
+                    textTransform={'uppercase'}
+                    mt={1}>
+                    {character.name}
+                  </Text>
+                </Box>
               ))}
-        </div>
+        </SimpleGrid>
       )}
     </Box>
   );
