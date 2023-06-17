13c50757dc8813c34d0b26111225840fb7e6546a
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-17 10:46:55 +0100

    Revert "feat: add i18n"
    This reverts commit 7cc431006d6bcb3da6da8dc3e0f53a2a09eba158.

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import './i18n'
 import MyRoutes from './routes/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- packages/web/src/routes/home/index.tsx
+++ packages/web/src/routes/home/index.tsx
@@ -1,15 +1,7 @@
-import { useTranslation } from 'react-i18next'
-
 function Home() {
-  const { t } = useTranslation(`home`)
   return (
     <div className="Home">
-      <header className="Home-header">
-        <p>
-          {t(`title`)}
-          {t(`description`)}
-        </p>
-      </header>
+      Home
     </div>
   )
 }
